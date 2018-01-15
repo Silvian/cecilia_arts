c6ba50f7474f046853eb91edd91dfb08777b7332
silvian <silvian.dragan@gmail.com>
2018-01-15 23:01:15 +0000

    registered models in admin panel

--- mailservice/admin.py
+++ mailservice/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Email, EmailConfiguration
+
+admin.site.register(Email)
+admin.site.register(EmailConfiguration)
