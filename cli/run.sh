85052307035bf4f6acc736a856bba02e1eb10c24
silvian <silvian.dragan@gmail.com>
2018-01-21 13:12:36 +0000

    dockerized the webapp

--- cli/run.sh
+++ cli/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sleep 5
-python manage.py runserver 0.0.0.0:8000
+sleep 2
+dockerize -wait tcp://postgres:5432 -wait tcp://rabbitmq:5672 -timeout 60s python manage.py runserver 0.0.0.0:8000
